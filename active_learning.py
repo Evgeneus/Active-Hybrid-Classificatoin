6c96f30762aa4d7f44bcdaf621badc2eedfd2cfa
Evgeny Krivosheev <K1r9i9v3osheev>
2018-07-19 19:14:29 +0200

    feature/ set random seed/ compute metrics

--- active_learning.py
+++ active_learning.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV, StratifiedKFold
+from sklearn.metrics import precision_recall_fscore_support
 from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import CountVectorizer, \
                                             TfidfTransformer, \
@@ -11,7 +12,6 @@ from sklearn.linear_model import LogisticRegression
 from modAL.uncertainty import uncertainty_sampling
 
 seed = 123
-# np.random.seed(seed)
 
 
 # class LinearSVC_proba(LinearSVC):
@@ -45,7 +45,8 @@ def do_active_learning(X, y, X_test, y_test, n_queries):
 
     # randomly select initial balanced training dataset
     init_train_size = 10
-    train_idx = np.concatenate([np.random.choice(pos_idx_all, init_train_size//2, replace=False),
+    np.random.seed(seed)
+    train_idx = np.concatenate([np.random.choice(pos_idx_all, init_train_size // 2, replace=False),
                                 np.random.choice(neg_idx_all, init_train_size // 2, replace=False)])
     X_train = X[train_idx]
     y_train = y[train_idx]
@@ -60,7 +61,6 @@ def do_active_learning(X, y, X_test, y_test, n_queries):
         X_training=X_train, y_training=y_train,
         query_strategy=uncertainty_sampling
     )
-    print('Accuracy before active learning: %f' % learner.score(X_test, y_test))
 
     # pool-based sampling
     for idx in range(n_queries):
@@ -72,13 +72,16 @@ def do_active_learning(X, y, X_test, y_test, n_queries):
         # remove queried instance from pool
         X_pool = np.delete(X_pool, query_idx, axis=0)
         y_pool = np.delete(y_pool, query_idx)
-        print('Accuracy after query no. %d: %f' % (idx + 1, learner.score(X_test, y_test)))
+
+        pre_, rec_, f1_, _ = precision_recall_fscore_support(y_test, learner.predict(X_test), average='binary')
+        print('F1 after query no. %d: %f' % (idx + 1, f1_), end='  ')
+        print('prop of + {:1.3f}'.format(sum(learner.y_training)/len(learner.y_training)))
 
 
 if __name__ == '__main__':
     df = pd.read_csv('./data/ohsumed_C14_C23_1grams.csv')
     predicate = 'C14'
-    n_queries = 30
+    n_queries = 300
 
     # load data
     X_, y_ = get_data(predicate, df)
