7e4657c9f770445d8e34a0c02e83cc1690d3285a
Evgeneus <evgeny.krivosheev@unitn.it>
2018-07-19 15:55:55 +0200

    active-learning/ refactoring

--- active_learning.py
+++ active_learning.py
@@ -10,15 +10,8 @@ from modAL.models import ActiveLearner
 from sklearn.linear_model import LogisticRegression
 from modAL.uncertainty import uncertainty_sampling
 
+seed = 123
 
-def get_data(predicate, df):
-    X_pos = df.loc[df[predicate] == 1]['tokens'].values
-    pos_num = X_pos.size
-    X_neg = df.loc[df[predicate] == 0]['tokens'].values[:pos_num]
-    X = np.append(X_pos, X_neg)
-    y = np.append(np.ones(pos_num), np.zeros(pos_num))
-
-    return X, y
 
 # class LinearSVC_proba(LinearSVC):
 #
@@ -32,23 +25,27 @@ def get_data(predicate, df):
 #         probs = platt_predictions / platt_predictions.sum(axis=1)[:, None]
 #         return probs
 
+def get_data(predicate, df):
+    X_pos = df.loc[df[predicate] == 1]['tokens'].values
+    pos_num = len(X_pos)
+#     X_neg = df.loc[df[predicate] == 0]['tokens'].values[:pos_num] # balanced data
+    X_neg = df.loc[df[predicate] == 0]['tokens'].values  # imbalanced data
+    neg_num = len(X_neg)
+    X = np.append(X_pos, X_neg)
+    y = np.append(np.ones(pos_num), np.zeros(neg_num))
 
-if __name__ == '__main__':
-    df = pd.read_csv('./data/ohsumed_C14_C23_1grams.csv')
-    predicate = 'C14'
+    return X, y
 
-    # load data
-    X_, y_ = get_data(predicate, df)
-    vectorizer = TfidfVectorizer(lowercase=False, max_features=1000, ngram_range=(1, 1))
-    X_ = vectorizer.fit_transform(X_).toarray()
+
+def do_active_learning(X, y , X_test, y_test, n_queries):
     # initial training data
     train_idx = [1, 2, 3, -1, -2, -3]
-    X_train = X_[train_idx]
-    y_train = y_[train_idx]
+    X_train = X[train_idx]
+    y_train = y[train_idx]
 
     # generating the pool
-    X_pool = np.delete(X_, train_idx, axis=0)
-    y_pool = np.delete(y_, train_idx)
+    X_pool = np.delete(X, train_idx, axis=0)
+    y_pool = np.delete(y, train_idx)
 
     # initializing the active learner
     learner = ActiveLearner(
@@ -56,10 +53,9 @@ if __name__ == '__main__':
         X_training=X_train, y_training=y_train,
         query_strategy=uncertainty_sampling
     )
-    print('Accuracy before active learning: %f' % learner.score(X_, y_))
+    # print('Accuracy before active learning: %f' % learner.score(X_, y_))
 
     # pool-based sampling
-    n_queries = 200
     for idx in range(n_queries):
         query_idx, query_instance = learner.query(X_pool, n_instances=1)
         learner.teach(
@@ -70,3 +66,16 @@ if __name__ == '__main__':
         X_pool = np.delete(X_pool, query_idx, axis=0)
         y_pool = np.delete(y_pool, query_idx)
         print('Accuracy after query no. %d: %f' % (idx + 1, learner.score(X_, y_)))
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('./data/ohsumed_C14_C23_1grams.csv')
+    predicate = 'C14'
+    n_queries = 200
+
+    # load data
+    X_, y_ = get_data(predicate, df)
+    vectorizer = TfidfVectorizer(lowercase=False, max_features=1000, ngram_range=(1, 1))
+    X_ = vectorizer.fit_transform(X_).toarray()
+
+    do_active_learning(X_, y_, None, None, n_queries)
