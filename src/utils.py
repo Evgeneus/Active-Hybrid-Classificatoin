77f066dbfb2838aa7612b635c76399d4f5e47a5c
Evgeneus <evgeny.krivosheev@unitn.it>
2018-07-24 21:00:56 +0200

    feature/ random_sampling

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-
+# load data from csv
 def load_data(predicate, df):
     X_pos = df.loc[df[predicate] == 1]['tokens'].values
     pos_num = len(X_pos)
@@ -10,3 +10,10 @@ def load_data(predicate, df):
     y = np.append(np.ones(pos_num), np.zeros(neg_num))
 
     return X, y
+
+
+# random sampling strategy for modAL
+def random_sampling(_, X, n_instances=1, seed=123):
+    np.random.seed(seed)
+    query_idx = np.random.randint(X.shape[0], size=n_instances)
+    return query_idx, X[query_idx]
