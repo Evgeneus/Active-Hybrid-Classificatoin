b776c78f787033725ee4592ddbae8bd3dca4b17d
Evgeny Krivosheev <K1r9i9v3osheev>
2018-10-10 11:35:32 +0200

    project structure & add data amazon sentiment reviews

---
+++ machine_and_experts_annotate/src/main.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import StratifiedKFold
+from sklearn.svm import LinearSVC
+from sklearn.calibration import CalibratedClassifierCV
+
+from machine_and_experts_annotate.src.utils import load_vectorize_data, transform_print, \
+    objective_aware_sampling, get_init_training_data_idx
+from machine_and_experts_annotate.src.active_learning import Learner, ScreeningActiveLearner
+
+seed = 123
+
+if __name__ == '__main__':
+    predicates = ['C04', 'C12']
+    file_name = 'ohsumed_C04_C12_1grams.csv'
+    # load and transform data
+    X, y_screening, y_predicate = load_vectorize_data(file_name, predicates, seed)
+
+    data_df = []
+    init_train_size = 20
+    k = 5
+    skf = StratifiedKFold(n_splits=k, random_state=seed)
+    for train_idx, test_idx in skf.split(X, y_screening):
+        print('-------------------------------')
+        # split training-test datasets
+        X_train, X_test = X[train_idx], X[test_idx]
+        y_screening_train, y_screening_test = y_screening[train_idx], y_screening[test_idx]
+        """create initial training dataset for all predicates
+           the dataset will be further used for fine tining screening out threshold """
+        # TO CHANGE TO VALIDATION DATA!!!!!!
+        init_train_idx = get_init_training_data_idx(y_screening_train, init_train_size, seed)
+
+        y_predicate_pool, y_predicate_train_init, y_predicate_test = {}, {}, {}
+        for pr in predicates:
+            y_predicate_pool[pr] = y_predicate[pr][train_idx]
+            y_predicate_train_init[pr] = y_predicate_pool[pr][init_train_idx]
+            y_predicate_pool[pr] = np.delete(y_predicate_pool[pr], init_train_idx)
+
+        X_train_init = X_train[init_train_idx]
+        X_pool = np.delete(X_train, init_train_idx, axis=0)
+        y_screening_init = y_screening_train[init_train_idx]
+        y_screening_train = np.delete(y_screening_train, init_train_idx)
+
+        # dict of active learners per predicate
+        learners = {}
+        for pr in predicates:  # setup predicate-based learners
+            learner_params = {
+                'clf': CalibratedClassifierCV(LinearSVC(class_weight='balanced', random_state=seed)),
+                'undersampling_thr': 0.333,
+                'seed': seed,
+                'p_out': 0.5,
+                'sampling_strategy': objective_aware_sampling,
+            }
+            learner = Learner(learner_params)
+
+            y_train_init = y_predicate[pr][train_idx][init_train_idx]
+            y_pool = np.delete(y_predicate[pr][train_idx], init_train_idx)
+            y_test = y_predicate[pr][test_idx]
+
+            learner.setup_active_learner(X_train_init, y_train_init, X_pool, y_pool, X_test, y_test)
+            learners[pr] = learner
+
+        screening_params = {
+            'n_instances_query': 200,  # num of instances for labeling for 1 query
+            'seed': seed,
+            'p_out': 0.7,
+            'lr': 5,
+            'beta': 3,
+            'learners': learners
+        }
+        SAL = ScreeningActiveLearner(screening_params)
+        # SAL.init_stat()  # initialize statistic for predicates, uncomment if use predicate selection feature
+        n_queries = 50
+        num_items_queried = init_train_size*len(predicates)
+        data = []
+
+        for i in range(n_queries):
+            # SAL.update_stat() # uncomment if use predicate selection feature
+            pr = SAL.select_predicate(i)
+            query_idx, query_idx_discard = SAL.query(pr)
+            SAL.teach(pr, query_idx, query_idx_discard)
+            # SAL.fit_meta(X_train_init, y_screening_init)
+
+            predicted = SAL.predict(X_test)
+            metrics = SAL.compute_screening_metrics(y_screening_test, predicted, SAL.lr, SAL.beta)
+            pre, rec, fbeta, loss, fn_count, fp_count = metrics
+            num_items_queried += SAL.n_instances_query
+            data.append([num_items_queried, pre, rec, fbeta, loss, fn_count, fp_count])
+
+            print('query no. {}: loss: {:1.3f}, fbeta: {:1.3f}, '
+                          'recall: {:1.3f}, precisoin: {:1.3f}'
+                  .format(i + 1, loss, fbeta, rec, pre))
+            data_df.append(pd.DataFrame(data, columns=['num_items_queried',
+                                                       'precision', 'recall',
+                                                       'f_beta', 'loss',
+                                                       'fn_count', 'fp_count']))
+
+    transform_print(data_df, learner_params['sampling_strategy'].__name__,
+                    predicates, 'screening_al_{}_{}XXX'.format(predicates[0], predicates[1]))
+    print('Done!')
---
+++ machine_and_experts_annotate/src/utils.py
@@ -0,0 +1,185 @@
+import numpy as np
+import pandas as pd
+import warnings, random
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import confusion_matrix
+
+
+# load and vectorize data
+def load_vectorize_data(file_name, predicates, seed):
+    df = pd.read_csv('../../data/ohsumed_data/{}'.format(file_name))
+    df_screening_pos = df.loc[df['Y'] == 1]
+    df_screening_neg = df.loc[df['Y'] == 0]
+
+    X_screening_pos = df_screening_pos['tokens'].values
+    X_screening_neg = df_screening_neg['tokens'].values
+    X = np.append(X_screening_pos, X_screening_neg)
+
+    y_screening = np.append(np.ones(X_screening_pos.shape[0]),
+                            np.zeros(X_screening_neg.shape[0]))
+
+    y_predicate = {}  # gt labels per predicate
+    for pr in predicates:
+        y_predicate[pr] = np.append(df_screening_pos[pr].values,
+                                    df_screening_neg[pr].values)
+
+    # vectorize and transform text
+    vectorizer = TfidfVectorizer(lowercase=False, max_features=2000, ngram_range=(1, 1))
+    X = vectorizer.fit_transform(X).toarray()
+
+    # shuffle X, y in unison
+    np.random.seed(seed)
+    idx = np.random.permutation(y_screening.shape[0])
+    X, y_screening = X[idx], y_screening[idx]
+    for pr in predicates:
+        y_predicate[pr] = y_predicate[pr][idx]
+
+    return X, y_screening, y_predicate
+
+# TO DO!!!
+def get_init_training_data_idx(y_screening, init_train_size, seed):
+   # initial training data
+   pos_idx_all = (y_screening == 1).nonzero()[0]
+   neg_idx_all = (y_screening == 0).nonzero()[0]
+   # randomly select initial balanced training dataset
+   np.random.seed(seed)
+   train_idx = np.concatenate([np.random.choice(pos_idx_all, init_train_size // 2, replace=False),
+                               np.random.choice(neg_idx_all, init_train_size // 2, replace=False)])
+
+   return train_idx
+
+
+# random sampling strategy for modAL
+def random_sampling(_, X, n_instances=1, seed=123):
+    random.seed(seed)
+    query_idx = np.array(random.sample(range(X.shape[0]), n_instances))
+
+    return query_idx, X[query_idx]
+
+
+# positive class certainty sampling strategy for modAL
+def positive_certainty_sampling(classifier, X, n_instances=1, **predict_proba_kwargs):
+    prob_in = classifier.predict_proba(X, **predict_proba_kwargs)[:, 1]
+    query_idx = np.argpartition(prob_in, -n_instances)[-n_instances:]
+
+    return query_idx, X[query_idx]
+
+
+# sampling takes into account conjunctive expression of predicates
+def objective_aware_sampling(classifier, X, learners_, n_instances=1, **uncertainty_measure_kwargs):
+    from modAL.uncertainty import classifier_uncertainty, multi_argmax
+    uncertainty = classifier_uncertainty(classifier, X, **uncertainty_measure_kwargs)
+    l_prob_in = np.ones(X.shape[0])
+    if learners_:
+        for l in learners_.values():
+            l_prob_in *= l.learner.predict_proba(X)[:, 1]
+        uncertainty_weighted = l_prob_in * uncertainty
+    else:
+        uncertainty_weighted = uncertainty
+
+    query_idx = multi_argmax(uncertainty_weighted, n_instances=n_instances)
+
+    return query_idx, X[query_idx]
+
+# # sampling takes into account conjunctive expression of predicates
+# def screening_sampling(classifier, X, learners_, n_instances=1, **uncertainty_measure_kwargs):
+#     from modAL.uncertainty import classifier_uncertainty, multi_argmax
+#     epsilon = 0.1
+#     uncertainty = classifier_uncertainty(classifier, X, **uncertainty_measure_kwargs)
+#
+#     # np.random.seed(seed=123)
+#     if np.random.binomial(1, epsilon):
+#         if learners_:
+#             for l in learners_.values():
+#                 uncertainty *= classifier_uncertainty(l.clf, X, **uncertainty_measure_kwargs)
+#         uncertainty_weighted = uncertainty
+#     else:
+#         l_prob_in = np.ones(X.shape[0])
+#         if learners_:
+#             for l in learners_.values():
+#                 l_prob_in *= l.learner.predict_proba(X)[:, 1]
+#             uncertainty_weighted = l_prob_in * uncertainty
+#         else:
+#             uncertainty_weighted = uncertainty
+#
+#     query_idx = multi_argmax(uncertainty_weighted, n_instances=n_instances)
+#
+#     return query_idx, X[query_idx]
+
+
+# screening metrics, aimed to obtain high recall
+class MetricsMixin:
+
+    @staticmethod
+    def compute_screening_metrics(gt, predicted, lr, beta):
+        '''
+        FP == False Inclusion
+        FN == False Exclusion
+        '''
+        fp = 0.
+        fn = 0.
+        tp = 0.
+        tn = 0.
+        for gt_val, pred_val in zip(gt, predicted):
+            if gt_val and not pred_val:
+                fn += 1
+            if not gt_val and pred_val:
+                fp += 1
+            if gt_val and pred_val:
+                tp += 1
+            if not gt_val and not pred_val:
+                tn += 1
+        loss = (fn * lr + fp) / len(gt)
+        try:
+            recall = tp / (tp + fn)
+            precision = tp / (tp + fp)
+            beta = beta
+            fbeta = (beta ** 2 + 1) * precision * recall / (recall + beta ** 2 * precision)
+        except ZeroDivisionError:
+            warnings.warn('ZeroDivisionError -> recall, precision, fbeta = 0., 0., 0')
+            recall, precision, fbeta = 0., 0., 0
+
+        return precision, recall, fbeta, loss, fn, fp
+
+    @staticmethod
+    def compute_tpr_tnr(gt, predicted):
+        tn, fp, fn, tp = confusion_matrix(gt, predicted).ravel()
+        TPR = tp / (tp + fn)  # sensitivity, recall, or true positive rate
+        TNR = tn / (tn + fp)  # specificity or true negative rate
+
+        return TPR, TNR
+
+
+# transfrom data from k-fold CV and print results in csv
+def transform_print(data_df, sampl_strategy, predicates, file_name):
+    # compute mean and std, and median over k-fold cross validation results
+    df_concat = pd.concat(data_df)
+    by_row_index = df_concat.groupby(df_concat.index)
+    df_means = by_row_index.mean()
+    df_std = by_row_index.std()
+    df_median = by_row_index.median()
+
+    # form dataframe for printing out in csv
+    df_to_print = df_means
+    df_to_print.columns = ['num_items_queried', 'precision_mean',
+                           'recall_mean', 'f_beta_mean', 'loss_mean',
+                           'fn_count_mean', 'fp_count_mean']
+
+    df_to_print['precision_median'] = df_median['precision']
+    df_to_print['recall_median'] = df_median['recall']
+    df_to_print['f_beta_median'] = df_median['f_beta']
+    df_to_print['loss_median'] = df_median['loss']
+    df_to_print['fn_count_median'] = df_median['fn_count']
+    df_to_print['fp_count_median'] = df_median['fp_count']
+
+    df_to_print['precision_std'] = df_std['precision']
+    df_to_print['recall_std'] = df_std['recall']
+    df_to_print['f_beta_std'] = df_std['f_beta']
+    df_to_print['loss_std'] = df_std['loss']
+    df_to_print['fn_count_std'] = df_std['fn_count']
+    df_to_print['fp_count_std'] = df_std['fp_count']
+
+
+    df_to_print['sampling_strategy'] = sampl_strategy
+    df_to_print.to_csv('../output/machines_and_experts/{}.csv'.format(file_name), index=False)
