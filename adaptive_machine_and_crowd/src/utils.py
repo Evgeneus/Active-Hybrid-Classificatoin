0aeb514825af4eeda774428867b8aff14abf4aeb
Evgeny Krivosheev <K1r9i9v3osheev>
2019-03-14 16:41:23 +0100

    fix/utils/random sampling

--- adaptive_machine_and_crowd/src/utils.py
+++ adaptive_machine_and_crowd/src/utils.py
@@ -120,7 +120,7 @@ def get_init_training_data_idx(y_screening, y_predicate_train, init_train_size):
 
 # random sampling strategy for modAL
 def random_sampling(_, X, n_instances=1):
-    query_idx = np.array(random.sample(range(X.shape[0]), n_instances))
+    query_idx = random.sample(range(X.shape[0]), n_instances)
 
     return query_idx, X[query_idx]
 
